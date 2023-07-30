cf39a8b99c5e8a6eb634a81581a0359c85984e3e
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-07-30 21:58:54 +0900

    [save]07302158

---
+++ practice/practice.rb
@@ -0,0 +1,2 @@
+naoko = [1,2,3]
+puts naoko.class
---
+++ practice/practice3.rb
@@ -0,0 +1,11 @@
+puts "Hashの中身を出力するプログラム"
+
+hash ={
+    name: 'Ruby',
+kana:"るびー",
+author: "まつもと ゆきひろ",   birthday:    '1993-02-24' }
+hash.each do |key,value|
+puts "#{ key } -> #{value }"
+end
+
+put "hell world"
