1faf87397775199f0bf662db24b8973c20ab43aa
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-08-07 17:30:47 +0900

    [study]08071730

--- progate/index.rb
+++ progate/index.rb
@@ -1,2 +1,22 @@
-puts "Hello World"
-puts "Hello ruby"
+# puts "Hello World"
+# puts "Hello ruby"
+
+# puts 2
+# puts 2 + 3
+# puts "2 + 3"
+
+# puts 3 * 5
+# puts 6 / 2
+# puts 9 % 2
+
+# puts "にんじゃ" + "わんこ"
+# puts "3" + "5"
+# puts 3 + 5
+
+
+# name = "John"
+# puts name
+# puts "name"
+
+name = "にんじゃわんこ"
+puts name
