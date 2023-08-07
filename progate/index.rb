1f0492afcbe8636b6d5b5838fd7481c1ee187f86
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-08-07 18:48:55 +0900

    [study]08071848

--- progate/index.rb
+++ progate/index.rb
@@ -18,5 +18,30 @@
 # puts name
 # puts "name"
 
-name = "にんじゃわんこ"
-puts name
+# name = "にんじゃわんこ"
+# puts name
+
+# number1 = 10
+# puts number1 + 3
+
+# number2 = 5
+# puts number1 + number2
+
+# text = "ProgateでRubyをマスターしましょう"
+# puts "Aさん、" + text
+# puts "Bさん、" + text
+# puts "Cさん、" + text
+
+# number = 2
+# puts number
+
+# number = number + 3
+# puts number
+
+# name = "佐藤"
+# puts "こんにちは#{name}さん"
+
+age = 19
+puts "#{age}歳です"
+
+
