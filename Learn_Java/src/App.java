2a8e6ed9cbaa02f164fe00ad45be928d8c4afd39
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-08-30 15:20:08 +0900

    [study]java08301520

--- Learn_Java/src/App.java
+++ Learn_Java/src/App.java
@@ -6,5 +6,6 @@ public class App {
         System.out.println(3 * 2);
         System.out.println(6 / 2);
         System.out.println(8 % 5);
+        System.out.println("8" + "5");
     }
 }
---
+++ Learn_Java/src/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+  public static void main(String[] args) throws Exception {
+    int number;
+    number = 10;
+    System.out.println(number);
+
+    
+}
+}
