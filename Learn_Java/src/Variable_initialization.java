95a230570566913711b4920f3688917a779a2558
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-08-31 00:24:56 +0900

    [study]java08310024

---
+++ Learn_Java/src/Variable_initialization.java
@@ -0,0 +1,10 @@
+public class Variable_initialization {
+  public static void main(String[] args) throws Exception {
+    int number = 10;
+    String text = "Hello";
+    System.out.println(number + 3);
+    System.out.println(text + "World");
+    String word = "World";
+    System.out.println(text + word);
+}
+}
