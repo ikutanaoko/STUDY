c774328296adaa4e1bc30d092d4c0f911c8b7174
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-08-31 21:20:05 +0900

    [study]java08312120

---
+++ Learn_Java/src/Variable_update.java
@@ -0,0 +1,16 @@
+public class Variable_update {
+  public static void main(String[] args) throws Exception {
+    int number = 10;
+    String text = "Hello";
+    System.out.println(number);
+    System.out.println(text);
+    
+    number = 15;
+    text = "Good";
+
+    System.out.println(number);
+    System.out.println(text);
+
+
+}
+}
