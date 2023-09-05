2c7d412618483ee63f4cb8405dd239c906ce8591
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-05 21:04:59 +0900

    [study]java09052104

--- Learn_Java/src/Double.java
+++ Learn_Java/src/Double.java
@@ -3,5 +3,6 @@ public class Double {
       double number1 = 5.28;
       double number2 = 3.72;
       System.out.println(number1 + number2);
+      System.out.println(number1 * number2);
     }
 }
