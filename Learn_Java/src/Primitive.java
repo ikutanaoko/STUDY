baef3e462256429dd026fe949a084bad8e132a9e
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-07 18:39:24 +0900

    [study]java09071839

--- Learn_Java/src/Primitive.java
+++ Learn_Java/src/Primitive.java
@@ -2,7 +2,9 @@ public class Primitive {
   public static void main(String[] args) throws Exception {
     int intValue = 123;
     long longValue = intValue;
+    short shortValue = (short)intValue;
     System.out.println(longValue);
     System.out.println(intValue);
+    System.out.println(shortValue);
   }
 }
