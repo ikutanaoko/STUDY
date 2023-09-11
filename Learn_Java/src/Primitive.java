16562b733e4815d7e2f3bee58ab6c9e3eba0ebeb
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-11 15:29:51 +0900

    [save]09111529

--- Learn_Java/src/Primitive.java
+++ Learn_Java/src/Primitive.java
@@ -2,11 +2,12 @@ public class Primitive {
   public static void main(String[] args) throws Exception {
     int intValue = 123;
     long longValue = intValue;
+    // 大きさが違うため互換不可
     // short shortValue = (short)intValue;
-    // short shortValue = (short)longValue;
+    short shortValue = (short)longValue;
     System.out.println(longValue);
     System.out.println(intValue);
-    // System.out.println(shortValue);
+    System.out.println(shortValue);
     
   }
 }
