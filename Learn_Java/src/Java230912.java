f752e7451f4faf4db89ce24e7e315c38166b126a
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-12 20:39:01 +0900

    [study]09122038

---
+++ Learn_Java/src/Java230912.java
@@ -0,0 +1,8 @@
+public class Java230912 {
+    public static void main(String[] args) throws Exception {
+        int a = 3;
+        if (a == 3) {
+            System.out.println("a is 3.");
+        }
+    }
+}
