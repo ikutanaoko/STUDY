a0e0cb25de2477237505abe8333803c828f9393c
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-13 18:30:34 +0900

    [study]java09131830

--- Learn_Java/src/Java230912_13.java
+++ Learn_Java/src/Java230912_13.java
@@ -1,8 +1,10 @@
-public class Java230912 {
+public class Java230912_13 {
     public static void main(String[] args) throws Exception {
-        int a = 3;
+        int a = 5;
         if (a == 3) {
             System.out.println("a is 3.");
+        } else {
+            System.out.println("a is not 3");
         }
     }
 }
