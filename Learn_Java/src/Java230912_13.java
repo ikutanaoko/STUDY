03a6a768e0008be5669421cad160f9e10664deb5
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-13 19:02:18 +0900

    [study]09131902

--- Learn_Java/src/Java230912_13.java
+++ Learn_Java/src/Java230912_13.java
@@ -1,10 +1,14 @@
 public class Java230912_13 {
     public static void main(String[] args) throws Exception {
-        int a = 5;
+        int a = 6;
         if (a == 3) {
             System.out.println("a is 3.");
+        } else if (a == 5) {
+            System.out.println("a is 5");
         } else {
-            System.out.println("a is not 3");
+            System.out.println("a is unknown");
+        
         }
+        
     }
 }
