b86595cb8a3424380ebc0a57ecda568e7aa0471f
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-14 20:44:29 +0900

    [study]java08142044

--- Learn_Java/src/Java230912_13.java
+++ Learn_Java/src/Java230912_13.java
@@ -1,14 +1,33 @@
 public class Java230912_13 {
     public static void main(String[] args) throws Exception {
-        int a = 6;
-        if (a == 3) {
-            System.out.println("a is 3.");
-        } else if (a == 5) {
-            System.out.println("a is 5");
-        } else {
-            System.out.println("a is unknown");
-        
-        }
-        
+        // [if]
+        // int a = 6;
+        // if (a == 3) {
+        //     System.out.println("a is 3.");
+        // } else if (a == 5) {
+        //     System.out.println("a is 5");
+        // } else {
+        //     System.out.println("a is unknown");
+        // }
+
+        // [for]
+        // int i;
+        // for (i = 0; i < 10; i++) {
+        // System.out.println(i);
+        // }
+
+        // [while]
+        // int i = 0;
+        // while (i < 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // [do while]
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i < 10);
     }
 }
