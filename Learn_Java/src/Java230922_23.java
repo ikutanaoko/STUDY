66a037a89ac0800156eff6cbfb6d658c357f7608
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-22 23:28:25 +0900

    [study]java09222328

---
+++ Learn_Java/src/Java230922_23.java
@@ -0,0 +1,22 @@
+public class Java230922_23 {
+    public static void main(String[] args) throws Exception {
+        int n = 3;
+        switch (n) {
+        case 1: 
+            System.out.println("n is one.");
+            break;
+        case 2:
+            System.out.println("n is two.");
+            break;
+        case 3:
+            System.out.println("n is three.");
+            break;
+        default:
+            System.out.println("unknown");
+            break;
+        }
+
+
+        }
+        
+}
