d877a5909a6e25349947cdd5440e013da119229a
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-24 16:00:39 +0900

    [study]java09241600

---
+++ Learn_Java/src/Java230924.java
@@ -0,0 +1,15 @@
+public class Java230924 {
+    public static void main(String[] args) throws Exception {
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("finish!");
+
+
+        }
+        
+}
