739a69eb6a870f3537a119754a5dba803e4aa80e
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-25 16:28:35 +0900

    [study]java09251628

---
+++ Learn_Java/src/Java230924_25.java
@@ -0,0 +1,28 @@
+public class Java230924_25 {
+    public static void main(String[] args) throws Exception {
+        // int i;
+        // for (i = 0; i < 10; i++) {
+        //     if (i == 5) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // System.out.println("finish!");
+
+        int a;
+        int j;
+        loop1:
+            for (a = 0; a < 10; a++ ) {
+                for (j = 0; j < 10; j++) {
+                    if ((a == 1) && (j == 5)) {
+                        break loop1;
+                    }
+                    System.out.println("a is" + a);
+                    System.out.println("j is" +j);
+                }
+                System.out.println("finish!");
+            }
+
+        }
+        
+}
