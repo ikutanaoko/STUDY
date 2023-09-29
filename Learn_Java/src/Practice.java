c912f9b49c8364bb0372f31689d2e7baae39dd4d
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-09-29 17:53:19 +0900

    [save]

--- Learn_Java/src/Practice.java
+++ Learn_Java/src/Practice.java
@@ -1,9 +1,9 @@
 public class Practice {
   public static void main(String[] args) throws Exception {
-    int number;
-    number = 10;
-    System.out.println(number);
-
+    // int number;
+    // number = 10;
+    // System.out.println(number);
+    System.out.println("Docker構築");
     
 }
 }
