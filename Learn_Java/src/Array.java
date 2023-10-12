038d6fcf496ac008d1e0cbd79d60785285fcb2bb
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-10-13 00:26:53 +0900

    [study]java10130026

---
+++ Learn_Java/src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+ public static void main(String[]args) {
+  int[] scores;
+  scores = new int[5];
+  System.out.println("---------------------------------------");
+  System.out.println("配列の数：" + scores.length);
+ }
+ 
+}
