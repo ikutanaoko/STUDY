b71a74e65f652805331ebcb8b083cf071ce8dc56
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-11-20 07:45:07 +0900

    save

---
+++ Learn_Java/src/Collabolation/Main.java
@@ -0,0 +1,91 @@
+// CollaborativeEditorServlet.java
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@WebServlet("/CollaborativeEditorServlet")
+public class CollaborativeEditorServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static Set<Session> sessions = new HashSet<>();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // クライアントからのリクエストを処理
+        String userId = request.getParameter("userId");
+        String editedText = request.getParameter("editedText");
+
+        // セッションを通じて接続中のクライアントにメッセージを送信
+        for (Session session : sessions) {
+            if (session.isOpen()) {
+                session.getBasicRemote().sendText(userId + "," + editedText);
+            }
+        }
+
+        response.setContentType("text/plain");
+        response.getWriter().write("OK");
+    }
+
+    @ServerEndpoint("/CollaborativeEditorSocket")
+    public static class CollaborativeEditorSocket {
+
+        @OnOpen
+        public void onOpen(Session session) {
+            // 新しいクライアントが接続されたときの処理
+            sessions.add(session);
+        }
+
+        @OnMessage
+        public void onMessage(String message, Session session) {
+            // クライアントからのメッセージを処理
+            for (Session s : sessions) {
+                if (s.isOpen() && !s.equals(session)) {
+                    // 他のクライアントにメッセージを送信
+                    try {
+                        String mergedText = mergeText(s, message);
+                        s.getBasicRemote().sendText(mergedText);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        @OnClose
+        public void onClose(Session session) {
+            // クライアントが切断されたときの処理
+            sessions.remove(session);
+        }
+
+        private String mergeText(Session session, String incomingMessage) throws IOException {
+            // 他のクライアントからのメッセージと、既存のエディタ内容をマージして返す
+            String[] parts = incomingMessage.split(",");
+            String senderId = parts[0];
+            String incomingText = parts[1];
+
+            String existingText = (String) session.getUserProperties().get("editorText");
+            if (existingText == null) {
+                existingText = "";
+            }
+
+            // 単純な文字列連結でマージする例（実際のアプリケーションでは適切なマージロジックを実装するべき）
+            String mergedText = existingText + incomingText;
+            
+            // マージした結果をセッションに保存
+            session.getUserProperties().put("editorText", mergedText);
+
+            return mergedText;
+        }
+    }
+}
+
--- Learn_Java/src/sample1.java
+++ Learn_Java/src/sample1.java
@@ -6,88 +6,17 @@ import java.util.Arrays;
 public class sample1 {
   public static void main(String[] args) {
     
-@WebServlet("/PostServlet")
-public class PostServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // フォームからのデータを取得
-        String postData = request.getParameter("postData");
-        int formId = Integer.parseInt(request.getParameter("formId"));
+    int[][] array = {{1, 2, 3}, {1, 2, 3}};
 
-        // DAOを使用してデータベースに新しい投稿を保存
-        PostDAO postDAO = new PostDAO();
-        postDAO.savePost(postData);
-
-        // DAOを使用して最新の投稿一覧を取得
-        List<Post> postList = postDAO.getAllPosts();
-
-        // 投稿一覧をJSONに変換
-        String jsonResponse = convertListToJson(postList);
-
-        // レスポンスにJSONデータを設定
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(jsonResponse);
-    }
-
-    private String convertListToJson(List<Post> postList) {
-        // 投稿一覧をJSONに変換するロジックを実装
-        // ここでは簡略化のため未実装
-        return "";
-    }
-}
-
-// Ajaxリクエストを行うJavaScript
-function postAndUpdate(formId) {
-  var postData = $("#postData" + formId).val();
-
-  $.ajax({
-      type: "POST",
-      url: "PostServlet",
-      data: { postData: postData, formId: formId },
-      dataType: "json", // サーバーからの応答の形式をJSONとして指定
-      success: function(response) {
-          // サーバーからの応答を受けて、特定の要素を更新
-          updatePostList(response, formId);
-      }
-  });
-}
-
-// 特定の要素を更新するJavaScript
-function updatePostList(postList, formId) {
-  // 投稿一覧を表示する部分のIDを指定して更新
-  $("#postList" + formId).empty(); // 一旦クリア
-  // 投稿一覧を再描画するロジックを実装
-  // ここでは簡略化のため未実装
-}
-
-// 特定の要素を更新するJavaScript
-function updatePostList(postList, formId) {
-  // 投稿一覧を表示する部分のIDを指定して更新
-  var postListContainer = $("#postList" + formId);
-  
-  // 一旦クリア
-  postListContainer.empty();
-
-  // 投稿一覧を再描画するロジックを実装
-  if (postList.length === 0) {
-      postListContainer.append("<p>No posts yet.</p>");
-  } else {
-      var ul = $("<ul>");
-
-      // 投稿一覧をリストアイテムに追加
-      for (var i = 0; i < postList.length; i++) {
-          var post = postList[i];
-          var li = $("<li>").text(post.content);
-          ul.append(li);
-      }
-
-      postListContainer.append(ul);
-  }
-}
+    int[] array2 = array[1].clone();
 
 
-
+    System.out.println((array[0][1]).equals(array2[1]));
+    // System.out.println(array.equals(array2));
+    
+    array2[1] = 3;
+    System.out.println(array[1]);
+    System.out.println(array2[1]);
 
 
 
